e3f963c5d750c21a420d52dabd6ac4aaa4ad60f5
Bozhidar Manev <bojomanev@gmail.com>
2023-11-30 23:23:53 +0100

    Matching on Different Errors
    using nested {match}

--- 09ErrorHandling/result/src/main.rs
+++ 09ErrorHandling/result/src/main.rs
@@ -8,4 +8,20 @@ fn main() {
         Ok(file) => file,
         Err(error) => panic!("Problem opening the file {:?}", error),
     };
+
+    // Matching on different Errors
+    let greeting_file_result = File::open("hello.txt");
+
+    let greeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => {
+                panic!("Problem opening the file: {:?}", other_error)
+            }
+        },
+    };
 }
