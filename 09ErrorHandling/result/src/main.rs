c41f468d4550c7c3f4d4ec1b1968e0896330232b
Bozhidar Manev <bojomanev@gmail.com>
2023-12-01 00:37:49 +0100

    Propagating  errors in many ways

--- 09ErrorHandling/result/src/main.rs
+++ 09ErrorHandling/result/src/main.rs
@@ -1,9 +1,12 @@
 #![allow(unused)]
 
-use std::{fs::File, io::ErrorKind};
+use std::{
+    fs::{self, File},
+    io::{self, Error, ErrorKind, Read},
+};
 fn main() {
     // Recoverable errors with Result
-        let greeting_file_result = File::open("hello.txt");
+    let greeting_file_result = File::open("hello.txt");
     let greeting_file = match greeting_file_result {
         Ok(file) => file,
         Err(error) => panic!("Problem opening the file {:?}", error),
@@ -39,4 +42,47 @@ fn main() {
     // Using expect instead
     let greeting_file =
         File::open("hello.txt").expect("hello.txt should be included in this project");
+
+    // Propagating errors
+    read_username_from_file();
+    read_username_from_file_short();
+    read_username_from_file_shorter();
+    read_username_from_file_shortest();
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let username_file_result = File::open("hello.txt");
+
+    let mut username_file = match username_file_result {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut username = String::new();
+
+    match username_file.read_to_string(&mut username) {
+        Ok(_) => Ok(username),
+        Err(e) => Err(e),
+    }
+}
+
+fn read_username_from_file_short() -> Result<String, Error> {
+    let mut username_file = File::open("hello.txt")?; 
+    // ? if Err will break and propagate
+    // Can only be used in functions with return type that implements FromResidual
+    let mut username = String::new();
+    username_file.read_to_string(&mut username)?;
+    Ok(username)
+}
+
+fn read_username_from_file_shorter() -> Result<String, Error> {
+    let mut username = String::new();
+
+    File::open("hello.txt")?.read_to_string(&mut username)?;
+    Ok(username)
+}
+
+fn read_username_from_file_shortest() -> Result<String, Error> {
+    fs::read_to_string("hello.txt")
+    // std provides a function that opens the file, creates a new String, reads the contents of the file, puts the contents into that String, and returns it.
 }
