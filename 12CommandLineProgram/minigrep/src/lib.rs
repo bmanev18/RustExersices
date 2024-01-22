3ab69a4ed129d97357e47731a28222ec1d7c0ba8
Bozhidar Manev <bojomanev@gmail.com>
2024-01-22 18:15:21 +0100

    Minigrep

---
+++ 12CommandLineProgram/minigrep/src/lib.rs
@@ -0,0 +1,42 @@
+use std::{error::Error, fs};
+
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+}
+
+impl Config {
+    // Typical constructor method
+    /* fn new(args: &[String]) -> Config {
+        if args.len() < 3 {
+            panic!("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Config { query, file_path }
+    } */
+
+    // {new} with improved error handling
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
+}
+
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents =
+        fs::read_to_string(config.file_path)?;
+
+    println!("With text:\n{contents}");
+
+    Ok(())
+}
