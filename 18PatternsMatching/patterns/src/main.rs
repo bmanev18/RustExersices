b514372e9dcb021635efb6aabd0c8cdb1113ab74
Bozhidar Manev <95079544+bmanev18@users.noreply.github.com>
2024-02-15 16:16:50 +0200

    Conditional if let expressions

--- 18PatternsMatching/patterns/src/main.rs
+++ 18PatternsMatching/patterns/src/main.rs
@@ -1,11 +1,31 @@
 fn main() {
-
     // {match} Arms
-{
-    let x = Some(5);
-    match x {
-        None=> None,
-        Some(i)=> Some(i+1),
-    };
-}
+    {
+        let x = Some(5);
+        match x {
+            None => None,
+            Some(i) => Some(i + 1),
+        };
+    }
+
+    // Conditional {if let} expressions
+    {
+        let favorite_color: Option<&str> = None;
+        let is_tuesday = false;
+        let age: Result<u8, _> = "34".parse();
+
+        if let Some(color) = favorite_color {
+            println!("Using your favorite color, {color}, as the background");
+        } else if is_tuesday {
+            println!("Tuesday is a green day!");
+        } else if let Ok(age) = age {
+            if age > 30 {
+                println!("Using purple as the backgroun color");
+            } else {
+                println!("Using orange as the background color");
+            }
+        } else {
+            println!("Using blue as the background color");
+        }
+    }
 }
