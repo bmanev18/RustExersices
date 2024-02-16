9940823786e2d4f8b20660a4e8827b392defb03b
Bozhidar Manev <95079544+bmanev18@users.noreply.github.com>
2024-02-16 13:31:15 +0200

    Matching Named Variables

--- 18PatternsMatching/pattern-syntax/src/main.rs
+++ 18PatternsMatching/pattern-syntax/src/main.rs
@@ -7,4 +7,18 @@ fn main() {
         3 => println!("three"),
         _ => println!("anything"),
     }
+
+    // Matching Named Variables
+    let x = Some(5);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(y) => println!("Matched, y = {y}"),
+        _ => println!("Defualt case, x = {:?}", x),
+        // {y} from {Some(y)} is only local to the match scope and shadows the outer {let y = 10}
+    }
+
+    println!("At the end: x = {:?}, y = {:?}", x, y);
+    // {y} in this case is the first {let y = 10}
 }
