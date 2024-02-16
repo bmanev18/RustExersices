bd4bbb79f6a19f957b9a95d1bd64cf87572a73e7
Bozhidar Manev <95079544+bmanev18@users.noreply.github.com>
2024-02-16 15:14:59 +0200

    Match Guards

--- 18PatternsMatching/pattern-syntax/src/main.rs
+++ 18PatternsMatching/pattern-syntax/src/main.rs
@@ -171,7 +171,34 @@ fn main() {
     match numbers {
         (first, .., last) => {
             println!("Some numbers: {first}, {last}");
-        }
-        // {..} can be used once and the usage should be clear
+        } // {..} can be used once and the usage should be clear
+    }
+
+    // Extra Conditions with Match Guards
+    let num = Some(4);
+
+    match num {
+        Some(x) if x % 2 == 0 => println!("The number {} is even", x),
+        Some(x) => println!("The number {} is odd", x),
+        None => (),
+    }
+
+    //using an external to the {match} variable, inside
+    let x = Some(5);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(n) if n == y => println!("Matched, n = {n}"),
+        _ => println!("Default case, x = {:?}", x),
+    }
+
+    //{|} with match guard
+    let x = 4;
+    let y = false;
+
+    match x {
+        4 | 5 | 6 if y => println!("yes"),
+        _ => println!("no"),
     }
 }
