db8833eb696d1ad925ebcd54a2262a3268c39125
Bozhidar Manev <95079544+bmanev18@users.noreply.github.com>
2024-02-16 13:21:23 +0200

    Matching literals

---
+++ 18PatternsMatching/pattern-syntax/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    // Matching literals
+    let x = 1;
+    match x {
+        1 => println!("one"),
+        2 => println!("two"),
+        3 => println!("three"),
+        _ => println!("anything"),
+    }
+}
