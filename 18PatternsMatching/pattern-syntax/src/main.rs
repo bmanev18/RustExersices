4ce185211f40969a299fbac4c3f9a3f58db57870
Bozhidar Manev <95079544+bmanev18@users.noreply.github.com>
2024-02-16 14:12:05 +0200

    Destructuring Enums

--- 18PatternsMatching/pattern-syntax/src/main.rs
+++ 18PatternsMatching/pattern-syntax/src/main.rs
@@ -66,4 +66,29 @@ fn main() {
         Point { x: 0, y } => println!("On the y axis at {y}"),
         Point { x, y } => println!("On neither axis"),
     }
+
+    // 2. Destructuring Enums
+    enum Message {
+        Quit,
+        Move { x: i32, y: i32 },
+        Write(String),
+        ChangeColor(i32, i32, i32),
+    }
+
+    let msg = Message::ChangeColor(0, 160, 255);
+
+    match msg {
+        Message::Quit => {
+            println!("The Quit variant has not data to destructure");
+        }
+        Message::Move { x, y } => {
+            println!("Move in the x direction {x} and in the y direction {y}");
+        }
+        Message::Write(text) => {
+            println!("Text message: {text}");
+        }
+        Message::ChangeColor(r, g, b) => {
+            println!("Change the color ro red {r}, green {g}, and blue {b}");
+        }
+    }
 }
